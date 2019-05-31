296527cea7bf7d84275f1c1c4c67c5fc84a37669
shaktigurung <mr.shakti.gurung@gmail.com>
2019-05-31 11:01:25 +1000

    Update mutation added

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -11,6 +11,13 @@ const  Mutation = {
         if(error) return error 
         return newPerson
     },
+    updatePerson: (parent, args) => {
+        const person = People.findOneAndUpdate({id: args.id, first: args.first, last: args.last});
+        if (!person) {
+          throw new Error(`Couldn’t find author with id ${id}`);
+        }
+        return person;
+    },
     deletePerson: (parent, args) => {
         return new Promise( (resolve, reject) => {
             People.findOneAndDelete(args.id, function(err, result){
